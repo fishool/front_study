2b351e59aee5395dfdbc25e03ed146b612fdfa7e
ijavacs <ijavacs@gmail.com>
2020-09-14 16:00:14 +0800

    study init

--- vue_study/src/components/HelloWorld.vue
+++ vue_study/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg | capitalize}}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li>
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your First Vue.js App'
     }
   }
 }
