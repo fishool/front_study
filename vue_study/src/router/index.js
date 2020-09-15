f15cc8c28c84b7c2586932235d293b8ed8db7d6d
ijavacs <ijavacs@gmail.com>
2020-09-15 15:29:23 +0800

    study init

--- vue_study/src/router/index.js
+++ vue_study/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Runoob from '@/components/runoob/Runoob'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Runoob',
+      component: Runoob
     }
   ]
 })
