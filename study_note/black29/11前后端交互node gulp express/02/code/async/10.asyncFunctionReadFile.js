316807b33c260eb2e2d7d6598b0390e95751c2e2
ijavacs <ijavacs@gmail.com>
2020-12-11 11:31:56 +0800

    11

--- study_note/black29/11前后端交互node gulp express/02/code/async/10.asyncFunctionReadFile.js
+++ study_note/black29/11前后端交互node gulp express/02/code/async/10.asyncFunctionReadFile.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 // 改造方法放在 util模块下
-// 改造现有异步函数api 让其返回promise对象 从而支持异步函数语法   
+// 改造现有异步函数api 让其返回promise对象 从而支持异步函数语法
 // readFile虽然是异步方法，但是其返回文件对象，所以无法返回默认Promise对象。 需要通过 promisefy 进行包装
 const promisify = require('util').promisify;
 // 调用promisify方法改造现有异步API 让其返回promise对象
 const readFile = promisify(fs.readFile);
 
 async function run () {
-	let r1 = await readFile('./1.txt', 'utf8')
-	let r2 = await readFile('./2.txt', 'utf8')
-	let r3 = await readFile('./3.txt', 'utf8')
+	let r1 = await readFile('./1.txt', 'utf8');
+	let r2 = await readFile('./2.txt', 'utf8');
+	let r3 = await readFile('./3.txt', 'utf8');
 	console.log(r1)
 	console.log(r2)
 	console.log(r3)
@@ -25,5 +25,5 @@ run();
  * clearTimeout()
  * setInterval()
  * clearInterval()
- * 
+ *
  */
--- study_note/black29/11前后端交互node gulp express/02/code/async/7.promise.js
+++ study_note/black29/11前后端交互node gulp express/02/code/async/7.promise.js
@@ -27,5 +27,5 @@ promise.then((result) => {
 	 console.log(result);
 })
 .catch((err)=> {
-	console.log(err);
+	console.log("错误");
 })
