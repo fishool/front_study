c35a4010cedbc147ae17468c58b057629016ab90
ijavacs <ijavacs@gmail.com>
2020-12-26 10:55:29 +0800

    11

--- study_note/black29/11前后端交互node gulp express/01/code/04.module.exports.js
+++ study_note/black29/11前后端交互node gulp express/01/code/04.module.exports.js
@@ -1,11 +1,11 @@
 const greeting = name => `hello ${name}`;
 const x = 100;
 // exports.x = x;
-module.exports.greeting = greeting;
+// module.exports.greeting = greeting;
 // 当exports对象和moudle.exports对象指向的不是同一个对象时 以module.exports为准
-// module.exports = {
-// 	name: 'zhangsan'
-// }
+module.exports = {
+	name: 'zhangsan'
+}
 
 // exports = {
 // 	age: 20
