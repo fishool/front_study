1f0952804add479097e8ddb0723af0ece90db2b5
S.C <ijavacs@gmail.com>
2021-01-11 17:25:17 +0800

    11

---
+++ study_note/javascript_base/zh.javascript.info/1.2.5.js
@@ -0,0 +1,17 @@
+console.log(typeof undefined);// "undefined"
+
+console.log(typeof 0) // "number"
+
+console.log(typeof 10n) // "bigint"
+
+console.log(typeof true) // "boolean"
+
+console.log(typeof "foo") // "string"
+
+console.log(typeof Symbol("id")) // "symbol"
+
+console.log(typeof Math )// "object"  (1)
+
+console.log(typeof null )// "object"  (2)
+
+console.log(typeof console.log )// "function"  (3)
