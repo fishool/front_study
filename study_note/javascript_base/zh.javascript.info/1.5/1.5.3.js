a2e07b9a30b467940ae300675fb7e8412d4cca19
S.C <ijavacs@gmail.com>
2021-05-13 15:27:51 +0800

    js

---
+++ study_note/javascript_base/zh.javascript.info/1.5/1.5.3.js
@@ -0,0 +1,16 @@
+/*
+   字符串
+   字符串的内部格式始终是 UTF-16，它不依赖于页面编码
+
+ */
+
+
+/*
+let str = `Hello`;
+// 第一个字符
+alert( str[0] ); // H
+alert( str.charAt(0) ); // H
+
+// 最后一个字符
+alert( str[str.length - 1] ); // o
+ */
