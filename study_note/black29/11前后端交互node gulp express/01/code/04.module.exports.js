99fd1807322f419906801a756e6fc5280e530b51
shichao <ijavacs@gmail.com>
2022-06-17 10:44:13 +0800

    vue

--- study_note/black29/11前后端交互node gulp express/01/code/04.module.exports.js
+++ study_note/black29/11前后端交互node gulp express/01/code/04.module.exports.js
@@ -5,7 +5,7 @@ exports.fuckyou = function() {
     console.log('fuckyou');
 }
 
-// module.exports  exports  默认是指向同一个对象
+// module.exports  exports  默认是指向同一个空对象
 // 最终对外暴露的对象 是module.exports
 console.log(module.exports)
 console.log(exports)
