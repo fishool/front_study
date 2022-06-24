e5345b1c49119ff40806aa7f28ce55a86e825ca7
石超 <ijavacs@gmail.com>
2022-06-24 16:57:38 +0800

    vue 注释

--- vue3/vue3-demo/src/App.vue
+++ vue3/vue3-demo/src/App.vue
@@ -1,16 +1,65 @@
 <template>
+  <p>当前鼠标x:{{mouse.x}}</p>
+  <p>当前鼠标x:{{mouse.y}}</p>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld :msg="msg"/>
+  <button @click="saySetup">Click me</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {onMounted, onUnmounted, reactive, ref} from "vue";
 
 export default {
   name: 'App',
+  // 1.setup组合API的起点
+  // 2.可以理解为beforeCreate钩子函数前执行
+  // 3.setup中不能使用this , 因为组件还没有实例化
+  // 4. 模板中需要使用的数据和函数, 需要在setup中返回
+  setup() {
+    console.log('setup start');
+
+    const msg = 'setup vue3';
+    // 挂载生命周期钩子函数
+    onMounted(() => {
+      console.log('mounted');
+    });
+
+    // 鼠标移动逻辑组件
+    // 定义响应式数据1
+    const mouse = reactive({
+      x: 0,
+      y: 0
+    });
+    // 定义响应式数据-单个数据 Ref.value
+    const refValue = ref(2);
+    console.log(refValue.value);
+
+    const handleMouseMove = (e) => {
+      mouse.x = e.clientX
+      mouse.y = e.clientY
+    };
+    onMounted(() => {
+      document.addEventListener('mousemove', handleMouseMove)
+    });
+    onUnmounted(() => {
+      document.removeEventListener('mousemove', handleMouseMove)
+    });
+    let saySetup = function () {
+      console.log('setup');
+      mouse.x = 0;
+      mouse.y = 0;
+    };
+
+    // 模板中需要使用的数据和函数 需要在setup中返回
+    return {msg, saySetup,mouse};
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 
