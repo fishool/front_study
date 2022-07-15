96f250b285ba64192de25747b547fff158bdb498
石超 <sc326860.jx@chinatelecom.cn>
2022-07-15 15:47:42 +0800

    websoket 引入

--- util/websocket/setting.js
+++ util/websocket/setting.js
@@ -5,6 +5,6 @@
 const env = process.env.NODE_ENV;
 
 const Setting = {
-    websocketUrl: env === 'development' ? 'ws://localhost:8080/pcWebSocket/' : 'ws://59.52.10.139:11090/pcWebSocket/',
+    websocketUrl: env === 'development' ? 'ws://localhost:8080/pcWebSocket/' : 'ws://59.52.10.19:11090/pcWebSocket/',
 };
 module.exports = Setting;
